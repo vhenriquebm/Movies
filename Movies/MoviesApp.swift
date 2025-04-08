83cf8d160f182150b2a93eec0d22a1f7686f9f43
vhenriquebm <vhenriquebm@gmail.com>
2025-04-07 21:23:22 -0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Vitor Henrique Barreiro Marinho on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesTests/MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Vitor Henrique Barreiro Marinho on 07/04/25.
+//
+
+import Testing
+@testable import Movies
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
